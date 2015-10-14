456b5e7de1f0eae6e42b043825c48037ef8480cd
Евгения Симонова <menicheka@example.com>
2015-10-14 15:09:28 +0700

    [wrike64777714] error correction

--- src/test/groovy/com/terminal/pages/OwnerPersonalAccountPage.groovy
+++ src/test/groovy/com/terminal/pages/OwnerPersonalAccountPage.groovy
@@ -7,8 +7,8 @@ class OwnerPersonalAccountPage extends Page {
     static at = { $("meta", name: "pageId").@content == "owner:personalAccount" }
 
     static content = {
-        terminalsListLink { $("div.user-menu li.item1 a") }
-        moneyLink { $("div.user-menu li.item2.money a") }
+        	terminalsListLink { $("div.user-menu li.item1 a") }
+        	moneyLink { $("div.user-menu li.item2.money a") }
 		statisticLink { $("div.user-menu li.item3 a")}
 		settingsLink{ $("div.user-menu li.item5 a")}
 		logoutLink{ $("div.span4.navbar.singin.user-top").find("a", text: "Выйти")}
@@ -22,8 +22,9 @@ class OwnerPersonalAccountPage extends Page {
 		errorTextarea{ $("div.text.alert.alert-error") }
 		supTextarea{ errorTextarea.next().next() }
 		sendBtn{ supportForm.find("input", type:"button") }
-		sendtext{ $("div.sending") }
-		sendAccept{ $("div.accept") }
-
+		sendAccept{ supportForm.next().next()}
+		sendAcceptA{ sendAccept.children() }
+		sendAcceptB{ sendAcceptA.next()}
+		sendAcceptC{ sendAcceptB.next()}
 	}
 }
