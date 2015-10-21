769fb71320ebcc86d4caf88719e5cff2736effd3
Евгения Симонова <menicheka@example.com>
2015-10-21 11:27:03 +0700

    [wrike66145203] corrections3

---
+++ target/test-classes/GebConfig.groovy
@@ -0,0 +1,18 @@
+/*
+	This is the Geb configuration file.
+	
+	See: http://www.gebish.org/manual/current/configuration.html
+*/
+
+
+import org.openqa.selenium.firefox.FirefoxDriver
+import org.openqa.selenium.chrome.ChromeDriver
+//import org.openqa.selenium.ie.InternetExplorerDriver
+import com.terminal.pages.StaticData
+
+//driver = { new FirefoxDriver() }
+driver = { new ChromeDriver() }
+//driver = { new InternetExplorerDriver() }
+
+
+baseUrl = StaticData.getServerName()
