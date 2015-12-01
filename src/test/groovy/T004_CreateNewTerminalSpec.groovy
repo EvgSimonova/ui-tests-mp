893cdeae7b1526efe9f0cdd1f09db28d8cea0296
unknown <TMikhaylov@tMikhaylov.ANDProject.ru>
2015-12-01 17:37:03 +0300

    CreateNewterminal new branch

--- src/test/groovy/T004_CreateNewTerminalSpec.groovy
+++ src/test/groovy/T004_CreateNewTerminalSpec.groovy
@@ -1,18 +1,17 @@
 
-
 import com.terminal.pages.MainPage
 import com.terminal.pages.OwnerPersonalAccountPage
 import com.terminal.pages.OwnerTerminalListPage
-import com.terminal.pages.UserPersonalAccountPage
-import geb.spock.GebReportingSpec
-import java.text.SimpleDateFormat
 import com.terminal.pages.StaticData
-
+import geb.spock.GebReportingSpec
 import org.openqa.selenium.Keys
+
+import java.util.concurrent.TimeUnit
 
 class T004_CreateNewTerminalSpec extends GebReportingSpec {
 
     def "can get to OwnerTerminalListPage and create new Terminal"() {
+		driver.manage().timeouts().pageLoadTimeout(60, TimeUnit.SECONDS);
         when:
         to MainPage
         at MainPage
@@ -44,51 +43,235 @@ class T004_CreateNewTerminalSpec extends GebReportingSpec {
 		
 		when:
 		addTerminalButton.click()
-		
+
+		//check for empty fields
 		then:
 		waitFor{createTerminalHolder.displayed}
 		waitFor{createTerminalDialog.displayed}
 		waitFor{createTerminalForm.displayed}
+		waitFor{saveTerminalButton.displayed}
+
+		when:
+		saveTerminalButton.click()
+
+		then:
+		waitFor{createTerminalHolder.displayed}
+		waitFor{createTerminalDialog.displayed}
+		waitFor{createTerminalForm.displayed}
+		waitFor{errorAlert.displayed}
+		waitFor{errorAlert.text() == "Координаты, Охват аудитории в будние дни, Охват аудитории в выходные дни, Стоимоcть Размещения, Наименование, Описание, Режим работы, Операционная система, Адрес, введено(ы) неверно"}
+		//check only filled address
 		waitFor{terminalAddressInput.displayed}
-			
-		
+
 		when:
 		StaticData.setTerminalAddress(terminalAddressInput)
 		findTerminalOnMapButton.click()
 		showMapLink.click()
-		
+
 		then:
 		waitFor{showMapDialog.displayed}
-		
+		waitFor{saveTerminalButton.displayed}
+
 		when:
 		closeMapDialogLink.click()
+		//withAlert(wait: true) { saveTerminalButton.click() } == "необходимо ввести цену терминала"
+		saveTerminalButton.click()
+
+		then:
+		waitFor{createTerminalForm.displayed}
+		waitFor{errorAlert.displayed}
+		waitFor{errorAlert.text() == "Охват аудитории в будние дни, Охват аудитории в выходные дни, Стоимоcть Размещения, Наименование, Описание, Режим работы, Операционная система, введено(ы) неверно"}
+
+		//check only filled address and name
+		when:
 		StaticData.setTerminalName(terminalNameInput)
+		saveTerminalButton.click()
+
+		then:
+		waitFor{createTerminalForm.displayed}
+		waitFor{createTerminalForm.displayed}
+		waitFor{errorAlert.displayed}
+		waitFor{errorAlert.text() == "Охват аудитории в будние дни, Охват аудитории в выходные дни, Стоимоcть Размещения, Описание, Режим работы, Операционная система, введено(ы) неверно"}
+
+		//check only filled address and name, description
+		when:
 		StaticData.setTerminalDescription(terminalDescriptionTextarea)
-		//todo: добавить выбор группы
+		saveTerminalButton.click()
+
+		then:
+		waitFor{createTerminalForm.displayed}
+		waitFor{errorAlert.displayed}
+		waitFor{terminalGroupSelect.displayed}
+		waitFor{errorAlert.text() == "Охват аудитории в будние дни, Охват аудитории в выходные дни, Стоимоcть Размещения, Режим работы, Операционная система, введено(ы) неверно"}
+
+		//terminal group
+		when:
+		terminalGroupSelect.click()
+
+		then:
+		waitFor{terminalGroupSelectInactive.displayed}
+		waitFor{terminalGroupSelectActive.displayed}
+
+		when:
+		if (terminalGroupSelectIndex == "0") {
+			terminalGroupSelectActive.click()
+		} else {
+			terminalGroupSelectInactive.click()
+		}
+
+		then:
+		waitFor {saveTerminalButton.displayed}
+
+		when:
+		saveTerminalButton.click()
+
+		then:
+		waitFor{createTerminalForm.displayed}
+		waitFor{terminalGroupSelect.displayed}
+		waitFor{errorAlert.displayed}
+		waitFor{errorAlert.text() == "Охват аудитории в будние дни, Охват аудитории в выходные дни, Стоимоcть Размещения, Режим работы, Операционная система, введено(ы) неверно"}
+		waitFor{terminalTypeSelect.displayed}
+
+		//terminal type
+		when:
+		terminalTypeSelect.click()
+
+		then:
+		waitFor {terminalTypeSelectMonitor.displayed}
+		waitFor {terminalTypeSelectSmartphone.displayed}
+		waitFor {terminalTypeSelectATM.displayed}
+
+		when:
+		if (terminalTypeSelectIndex == '2') {
+			terminalTypeSelectMonitor.click()
+		} else {
+			terminalTypeSelectSmartphone.click()
+		}
+
+		then:
+		waitFor {saveTerminalButton.displayed}
+
+		when:
+		saveTerminalButton.click()
+
+		then:
+		waitFor{createTerminalForm.displayed}
+		waitFor{errorAlert.displayed}
+		waitFor{errorAlert.text() == "Охват аудитории в будние дни, Охват аудитории в выходные дни, Стоимоcть Размещения, Режим работы, Операционная система, введено(ы) неверно"}
+		waitFor{sliderLeft.displayed}
+		waitFor{sliderRight.displayed}
+
+		//the movement of the slider
+		when:
+		sliderLeft << Keys.ARROW_LEFT + Keys.ARROW_LEFT + Keys.ARROW_LEFT + Keys.ARROW_LEFT + Keys.ARROW_LEFT + Keys.ARROW_LEFT + Keys.ARROW_LEFT
+		sliderRight << Keys.ARROW_RIGHT + Keys.ARROW_RIGHT + Keys.ARROW_RIGHT + Keys.ARROW_RIGHT + Keys.ARROW_RIGHT + Keys.ARROW_RIGHT + Keys.ARROW_RIGHT
+		saveTerminalButton.click()
+
+		then:
+		waitFor{createTerminalForm.displayed}
+		waitFor{errorAlert.displayed}
+		waitFor{errorAlert.text() == "Охват аудитории в будние дни, Охват аудитории в выходные дни, Стоимоcть Размещения, Режим работы, Операционная система, введено(ы) неверно"}
+		waitFor{weekAudienceInput.displayed}
+
+		//check outreach weekdays 1.the importance of outreach during the week
+		when:
+		weekAudienceInput << "999999999999999999999999999999999999999999999999999999999999999"
+		saveTerminalButton.click()
+
+		then:
+		waitFor{createTerminalForm.displayed}
+		waitFor{errorAlert.displayed}
+		waitFor{weekAudienceInput.displayed}
+		waitFor{errorAlert.text() == "Охват аудитории в будние дни, Охват аудитории в выходные дни, Стоимоcть Размещения, Режим работы, Операционная система, введено(ы) неверно"}
+
+		//check outreach weekdays 2.an adequate value of outreach during the week
+		when:
+		weekAudienceInput << Keys.chord(Keys.CONTROL, "a") + Keys.DELETE
+		weekAudienceInput << "1000"
+		saveTerminalButton.click()
+
+		then:
+		waitFor{createTerminalForm.displayed}
+		waitFor{errorAlert.displayed}
+		waitFor{weekendAudienceInput.displayed}
+		waitFor{errorAlert.text() == "Охват аудитории в выходные дни, Стоимоcть Размещения, Режим работы, Операционная система, введено(ы) неверно"}
+
+		//test coverage on the weekend 1.the importance
+		when:
+		weekendAudienceInput << "999999999999999999999999999999999999999999999999999999999999999"
+		saveTerminalButton.click()
+
+		then:
+		waitFor{createTerminalForm.displayed}
+		waitFor{errorAlert.displayed}
+		waitFor{weekendAudienceInput.displayed}
+		waitFor{errorAlert.text() == "Охват аудитории в выходные дни, Стоимоcть Размещения, Режим работы, Операционная система, введено(ы) неверно"}
+
+		//test coverage on the weekend 2.an adequate value
+		when:
+		weekendAudienceInput << Keys.chord(Keys.CONTROL, "a") + Keys.DELETE
+		weekendAudienceInput << "23000"
+		saveTerminalButton.click()
+
+		then:
+		waitFor{createTerminalForm.displayed}
+		waitFor{errorAlert.displayed}
+		waitFor{startWorkTimeInput.displayed}
+		waitFor{errorAlert.text() == "Стоимоcть Размещения, Режим работы, Операционная система, введено(ы) неверно"}
+
+		//fill in the start time of the terminal
+		when:
+		startWorkTimeInput << "8:00"
+		saveTerminalButton.click()
+
+		then:
+		waitFor{createTerminalForm.displayed}
+		waitFor{errorAlert.displayed}
+		waitFor{endWorkTimeInput.displayed}
+		waitFor{errorAlert.text() == "Стоимоcть Размещения, Режим работы, Операционная система, введено(ы) неверно"}
+
+		//fill in the end time of the terminal
+		when:
+		endWorkTimeInput << "23:00"
+		saveTerminalButton.click()
+
+		then:
+		waitFor{createTerminalForm.displayed}
+		waitFor{errorAlert.displayed}
+		waitFor{operationSystemInput.displayed}
+		waitFor{errorAlert.text() == "Стоимоcть Размещения, Операционная система, введено(ы) неверно"}
+
+		//fill OS
+		when:
+		operationSystemInput << "Linux Mint"
+		saveTerminalButton.click()
+
+		then:
+		waitFor{createTerminalForm.displayed}
+		waitFor{errorAlert.displayed}
+		waitFor{costInput.displayed}
+		waitFor{errorAlert.text() == "Стоимоcть Размещения, введено(ы) неверно"}
+
+		// The cost of accommodation
+		when:
 		audienceAgeBeginInput<< Keys.chord(Keys.BACK_SPACE)
 		audienceAgeBeginInput<< "3"
 		audienceAgeEndInput<< Keys.chord(Keys.BACK_SPACE)
 		audienceAgeEndInput<< Keys.chord(Keys.BACK_SPACE)
 		audienceAgeEndInput<< "83"
-		//todo: добавить движение ползунками возраста
-		
-		weekAudienceInput<< "1000"
-		weekendAudienceInput<< "23000"
-		startWorkTimeInput<< "8:00"
-		endWorkTimeInput<< "23:00"
-		operationSystemInput<< "Linux Mint"
-		costInput<< "500"
+		costInput << "500"
 		saveTerminalButton.click()
-		
+
 		then:
 		waitFor{at OwnerTerminalListPage}
-		waitFor{terminalName.displayed}
+		//waitFor{terminalName.displayed}
 		waitFor{terminalsListLink.displayed}
 		waitFor{moneyLink.displayed}
 		waitFor{settingsLink.displayed}
 		waitFor{logoutLink.displayed}
 		waitFor{addTerminalButton.displayed}
-		
+		waitFor{terminalTable.displayed}
+
     }
 	
 	
--- src/test/groovy/com/terminal/pages/OwnerTerminalListPage.groovy
+++ src/test/groovy/com/terminal/pages/OwnerTerminalListPage.groovy
@@ -1,39 +1,41 @@
 package com.terminal.pages
-
 import java.text.SimpleDateFormat
-import com.terminal.pages.StaticData
-import geb.Page
 
 class OwnerTerminalListPage extends OwnerPersonalAccountPage {
-    //todo добавить id на страницу
+	//todo добавить id на страницу
 	//todo добавить в переменные терминал над которым производятся действия
-    static url = StaticData.getServerName()+"/owner/terminals" 
-	 static at = { title == "Mark project" }
-	
-	
-    static content = {
+	static url = StaticData.getServerName()+"/owner/terminals"
+	static at = { title == "Mark project" }
+
+
+	static content = {
 		addTerminalButton{ $("a.btn")}
-		
+		terminalsListLink { $("div.user-menu li.item1 a") }
+		moneyLink { $("div.user-menu li.item2.money a") }
+		settingsLink{ $("div.user-menu li.item5 a")}
+		logoutLink{ $("div.span4.navbar.singin.user-top").find("a", text: "Выйти")}
+		terminalTable{ $("table#table.sortable.terminal-table.tablesorter.tablesorter-blue.hasFilters")}
+
 		//for create new terminal group and for rename terminal group
 		groupMenuButton{ $("div.group-menu")}
 		createGroupLink{ $("div.group-menu li.item-1 a")}
 		renameGroupLink {$("div.group-menu li.item-2 a")}
 		balancePageLink{ $("div.user-menu li.item2.money a")}
-		
+
 		createGroupHolder{ $("div.group-action-holder.item-1")}
 		createGroupDialog{ createGroupHolder.children()}
 		groupNameInput{ createGroupDialog.find("input", id: "newGroupName", type: "text")}
 		createNewGroupButton{ createGroupDialog.find("input", class: "btn", type: "button")}
-		
+
 		renameGroupHolder{ $("div.group-action-holder.item-2")}
 		renameGroupDialog{ renameGroupHolder.children()}
 		renameGroupNameInput{renameGroupDialog.find("input", id: "editGroupName", type: "text")}
 		renameGroupButton{ renameGroupDialog.find("input", class: "btn", type: "button")}
-		
+
 		groupList{ $("div.image-menu")}
 		group1Link{ groupList.find("a", text: contains("Группа терминалов 1 - "+new SimpleDateFormat("yyyyMMdd").format(new Date())))}
 		renamedGroup1Link{ groupList.find("a", text: contains("Переименованная группа терминалов 1"))}
-		
+
 		//for add new terminal
 		createTerminalHolder{ $("div.edit-holder.create")}
 		createTerminalDialog{ createTerminalHolder.find("div.edit-box")}
@@ -46,7 +48,17 @@ class OwnerTerminalListPage extends OwnerPersonalAccountPage {
 		terminalNameInput{ createTerminalForm.find("input", name:"name", type: "text")}
 		terminalDescriptionTextarea{ createTerminalForm.find("textarea", name:"description", id: "description")}
 		terminalGroupSelect{ createTerminalForm.find("select", name:"terminalGroup")}
+		terminalGroupSelectIndex{ terminalGroupSelect.getAttribute("selectedIndex")}
+		terminalGroupSelectInactive{ terminalGroupSelect.children()}
+		terminalGroupSelectActive{ terminalGroupSelect.children().next()}
 		terminalTypeSelect{ createTerminalForm.find("select", name:"terminalType")}
+		terminalTypeSelectIndex{ terminalTypeSelect.getAttribute("selectedIndex")}
+		terminalTypeSelectMonitor{ terminalTypeSelect.children()}
+		terminalTypeSelectATM{ terminalTypeSelect.children().next()}
+		terminalTypeSelectSmartphone{ terminalTypeSelect.children().next().next()}
+		sliderRange{ $("div#slider-range.ui-slider.ui-slider-horizontal.ui-widget.ui-widget-content.ui-corner-all")}
+		sliderLeft{ sliderRange.children().next()}
+		sliderRight{ sliderRange.children().next().next()}
 		audienceAgeBeginInput{ createTerminalForm.find("input", type:"text", id:"amount")}
 		audienceAgeEndInput{ createTerminalForm.find("input", type:"text", id:"amount1")}
 		weekAudienceInput{ createTerminalForm.find("input", type:"text", id:"weekdayAudience")}
@@ -56,22 +68,16 @@ class OwnerTerminalListPage extends OwnerPersonalAccountPage {
 		operationSystemInput{createTerminalForm.find("input", type:"text", id:"operationSystem")}
 		costInput{createTerminalForm.find("input", type:"text", id:"cost")}
 		saveTerminalButton{createTerminalForm.find("input", type:"button", class:"btn", id:"saveButton")}
-		
+		errorAlert{ createTerminalForm.find("div", class:"alert alert-error", id:"create_error")}
+
 		terminalPane{ $("div.jspPane")}
 		terminalName{ terminalPane.find("h3", text: contains("Moscow"))}
-		
+
 		//for stop terminal
 		stopTerminalLink{ terminalPane.find("a.pause")}
 		stopTerminalHolder{ $("div.group-action-holder.item-6")}
 		stopTerminalDialog{ stopTerminalHolder.find("div.group-action").find("div.title", text: "Остановить терминал")}
 		acceptStopTerminalButton{ stopTerminalHolder.find("a.delete")}
 		runTerminalLink{ terminalName.parent().parent().find("a.start")}
-
-		//links
-		AccountPageLink{ $("div.span4.navbar.singin.user-top").children()}
-		logoutLink{ AccountPageLink.next()}
-		logoLink{ $("div.span8.logo").children().children()}
-		moneyLink { $("div.user-menu li.item2.money a") }
-		settingsLink{ $("div.user-menu li.item5 a")}
-    }
+	}
 }
