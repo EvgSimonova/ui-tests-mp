2bdc47e2f9d8e41b4973a6494b7833e60fd124fd
Евгения Симонова <menicheka@example.com>
2016-04-19 23:52:11 +0700

    [StaticData] downloadPictureDropBox

--- src/test/groovy/com/terminal/pages/StaticData.groovy
+++ src/test/groovy/com/terminal/pages/StaticData.groovy
@@ -168,15 +168,10 @@ def static downloadPictures(driverThis,nameImage) {
 	WebDriverWait wait = new WebDriverWait(driverThis, 300)
 	driverThis.currentUrl == "http://terminal-company.herokuapp.com/member/userImages"
 	waitVisibaly(driverThis.findElement(By.id("total-content-counter")), driverThis)
-	if (driverThis.findElements(By.id("multipartFile")).size() == 0){
-		wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("multipartFile")))
+	if (driverThis.findElements(By.className("dropin-btn-status")).size() == 0){
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("dropin-btn-status")))
 	}
-	/*if (driverThis.findElements(By.id("multipartFile")).size() == 0){
-		wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("multipartFile")))
-	}
-	driverThis.executeScript("document.getElementById('imageUploadForm').children[0].removeAttribute('class');")
-	driverThis.executeScript("document.getElementById('uniform-multipartFile').removeAttribute('class');")
-	driverThis.executeScript("document.getElementById('multipartFile').removeAttribute('style');")*/
+
 	if (driverThis.findElement(By.id("total-content-counter")).text != "Загруженные изображения (0)") {
 		driverThis.findElement(By.id("all-images")).findElement(By.tagName("li")).displayed
 	}
@@ -188,18 +183,39 @@ def static downloadPictures(driverThis,nameImage) {
 		idOld = Integer.valueOf(ourPisture.getAttribute("data-image-id"))
 	} else { idOld = 0 }
 
-	/*try {
-		driverThis.findElement(By.id("multipartFile")).with {
-			clear()
-			sendKeys(getDirImage() + nameImage)
+	try {
+		String winHandleBefore = driverThis.getWindowHandle()
+		driverThis.findElement(By.className("dropin-btn-status")).click()
+		for(String winHandle:driverThis.getWindowHandles()){
+			if (driverThis.title == "Sign into Dropbox" || driverThis.title == "Войти в Dropbox" ) {
+				driverThis.switchTo().window(winHandle)
+				assert driverThis.currentUrl.startsWith("https://www.dropbox.com/chooser?origin=")
+				waitVisibaly(driverThis.findElement(By.className('clearfix credentials-form login-form'), driverThis))
+				def login_mail = driverThis.findElement(By.xpath("//input[@name = 'login_email']"))
+				setUserEmailDropbox(login_mail)
+				def login_password = driverThis.findElement(By.xpath("//input[@name = 'login_password']"))
+				setUserEmailDropbox(login_password)
+				driverThis.findElement(By.xpath("//button[@textContent = 'ВойтиПродолжить']")).click()
+			}
+			waitVisibaly(driverThis.findElement(By.id("recent-file-list")),driverThis)
+			driverThis.findElement(By.className("text-input-input autofocus")).with {
+				clear()
+				sendKeys(nameImage)
+			}
+			waitVisibaly(driverThis.findElement(By.id("search-file-list")),driverThis)
+			def searchOurLi = driverThis.findElement(By.id('search-file-list"'))
+			searchOurLi.findElement(By.tagName("LI")).click()
+			searchOurLi.displayed
+			assert driverThis.findElement(By.xpath("//li[@class = 'dropin-file  selectable selected']"))
+			driverThis.findElement(By.id("select-btn")).click()
+			driverThis.close()
+			driverThis.switchTo().window(winHandleBefore)
 		}
 
 	} catch (WebDriverException e) {
 		println e
 
-	} catch (ElementNotVisibleException e) {
-		println e
-	}*/
+	}
 
 	try {
 		if (driverThis.findElements(By.id("fancybox-loading")).size() > 0) {
@@ -448,3 +464,23 @@ def static BalanceCampaignSpec(driverThis,sumCompany) {
 	assert Float.valueOf(sumText.substring(0,sumText.indexOf(' ')).replace(",", ".")) >= Float.valueOf(sumCompany)
 
 }
+
+/*if (driverThis.findElements(By.id("multipartFile")).size() == 0){
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("multipartFile")))
+	}
+	driverThis.executeScript("document.getElementById('imageUploadForm').children[0].removeAttribute('class');")
+	driverThis.executeScript("document.getElementById('uniform-multipartFile').removeAttribute('class');")
+	driverThis.executeScript("document.getElementById('multipartFile').removeAttribute('style');")
+
+	try {
+		driverThis.findElement(By.id("multipartFile")).with {
+			clear()
+			sendKeys(getDirImage() + nameImage)
+		}
+
+	} catch (WebDriverException e) {
+		println e
+
+	} catch (ElementNotVisibleException e) {
+		println e
+	}*/
