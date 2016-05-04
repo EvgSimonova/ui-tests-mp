68f0082f8ed0ed785812db9dabd07336a0637ad2
mptester2 <timurShneider@ya.ru>
2016-05-04 07:56:28 +0000

    T001_RegisterSpec.groovy edited online with Bitbucket. Added console output for 1-st function

--- src/test/groovy/T001_RegisterSpec.groovy
+++ src/test/groovy/T001_RegisterSpec.groovy
@@ -9,24 +9,30 @@ class T001_RegisterSpec extends GebReportingSpec {
 
     def "register as user with password's autogeneration"() {
         when:
+        println('Start')
         to MainPage
         at MainPage
         registrationLink.click()
+        println('registration link clicked')
 
         then:
         waitFor{at MainPage}
         waitFor{registrationDialog.displayed}
         waitFor{form.displayed}
+        println('at Main page and reg dialog displayed')
 
         when:
+        println('start filling Form')
         generatePasswordCheckbox = true
         roleSelect = "user"
         usernameInput << "mptestuser" + new SimpleDateFormat("yyyyMMddHHmmss").format(new Date()) + "@ya.ru"
         registerButton.click()
+        println('registration button clicked')
 
         then:
         waitFor{messageBox.displayed}
         waitFor{messageBox.text().trim() == "Вы были удачно зарегистрированы. Для входа на сайт проверьте вашу почту."}
+        println('registration message displayed')
     }
 
     def "register as user with password"() {
