8734d6edc5620e95081e4f6dd77fa913abd3dd2d
Евгения Симонова <menicheka@example.com>
2016-06-14 12:10:49 +0700

    [wrike91133313] create new test

---
+++ src/test/groovy/T111_LoadingTerminalPicturesOwnerSpec.groovy
@@ -0,0 +1,77 @@
+import com.terminal.pages.MainPage
+import com.terminal.pages.OwnerPersonalAccountPage
+import com.terminal.pages.OwnerTerminalListPage
+import com.terminal.pages.StaticData
+import geb.spock.GebReportingSpec
+
+import java.text.SimpleDateFormat
+
+class T111_LoadingTerminalPicturesOwnerSpec  extends GebReportingSpec {
+
+    private String nameTerminal
+
+    def "can get to Terminal list page and Loading the Pictures of terminal"() {
+        when:
+        println("go to Main page and login")
+        to MainPage
+
+        then:
+        waitFor {loginLink.displayed}
+
+        when:
+        at MainPage
+        loginLink.click()
+
+        then:
+        waitFor { at MainPage }
+        waitFor { loginDialog.displayed }
+
+        when:
+        usernameInputOnLoginForm << StaticData.getOwner1Name()
+        passwordInputOnLoginForm << StaticData.getOwner1Password()
+        loginButton.click()
+
+        then:
+        waitFor { at OwnerPersonalAccountPage }
+        waitFor { terminalsListLink.displayed }
+
+        when:
+        println("go to terminal list page")
+        terminalsListLink.click()
+
+        then:
+        waitFor { at OwnerTerminalListPage }
+        waitFor { addTerminalButton.displayed }
+
+        when:
+        println("Add new terminal")
+        nameTerminal = "Тестовый автоматический терминал " + new SimpleDateFormat("dd.MM.yyyy HH:mm:ss").format(new Date())
+        StaticData.AddNewTerminalOwnerSpec(driver, nameTerminal)
+
+        then:
+        waitFor { at OwnerTerminalListPage }
+        waitFor { cartCurrentTerminal.displayed }
+        waitFor { filtrName.displayed }
+
+        when:
+        filtrName << nameTerminal
+
+        then:
+        waitFor { cartCurrentTerminal.displayed }
+
+        when:
+        cartCurrentTerminal.find{it.nameCampaign == nameTerminal}.detailTerminal.click()
+
+        then:
+        waitFor { activeTerminal.displayed }
+
+        when:
+        driverThis.executeScript("document.getElementById('uniform-undefined').removeAttribute('class');")
+        driverThis.executeScript("document.getElementById('multipartFile').removeAttribute('style');")
+
+        then:
+        waitFor { activeTerminal.displayed }
+
+
+    }
+}
